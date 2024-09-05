5e32d4157bb46b0e6da85e6c84d3220fd2b5abde
jsolanoo <n00443072@upn.pe>
2024-09-05 09:35:57 -0500

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Random r = new Random();
+int num = r.Next(1, 6 + 1);
+Console.WriteLine($"Salio {num}");
+
+if (num == 1)
+{
+    Console.WriteLine("Perdio");
+}
+else if (num == 2)
+{
+    Console.WriteLine("Poco");
+}
+else if (num == 3)
+{
+    Console.WriteLine("Algo");
+}
+else
+{
+    Console.WriteLine("no se");
+}
+
+switch (num)
+{
+    case 4:
+    case 5:
+        Console.WriteLine("es 4 o 5");
+        break;
+    default:
+
+}
